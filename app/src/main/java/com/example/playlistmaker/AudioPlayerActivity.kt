17a9282f4c4a5b0e87b6e1451a0a7ba765d7eb20
Sin.Senya <sinitcyn.dmitriy@mail.ru>
2024-05-23 10:27:14 +0500

    Дз спринт 14. Debounce поиск и клик трека. Воспроизведение музыки.

--- app/src/main/java/com/example/playlistmaker/AudioPlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/AudioPlayerActivity.kt
@@ -1,10 +1,14 @@
 package com.example.playlistmaker
 
+import android.media.MediaPlayer
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.isVisible
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import java.text.SimpleDateFormat
@@ -13,14 +17,34 @@ import java.util.Locale
 const val PARCEL_KEY="track"
 
 class AudioPlayerActivity : AppCompatActivity() {
+    companion object {
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
+    }
+
+    private var playerState = STATE_DEFAULT
+    private var mediaPlayer = MediaPlayer()
+    private lateinit var playButton : ImageView
+    private lateinit var pauseButton : ImageView
+    private lateinit var backArrow : ImageView
+    private lateinit var trackProgress : TextView
+
+    private val handler = Handler(Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_audio_player)
 
-        val backArrow = findViewById<ImageView>(R.id.backButton)
+        playButton = findViewById<ImageView>(R.id.playButton)
+        pauseButton = findViewById<ImageView>(R.id.pauseButton)
+        backArrow = findViewById<ImageView>(R.id.backButton)
+
         backArrow.setOnClickListener{
             finish()
         }
+
 
         val track : Track? = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
             intent.getParcelableExtra(PARCEL_KEY, Track::class.java)
@@ -55,5 +79,78 @@ class AudioPlayerActivity : AppCompatActivity() {
 
         val trackCountryView = findViewById<TextView>(R.id.trackCountry)
         trackCountryView.text = track?.country
+
+        trackProgress = findViewById(R.id.trackProgress)
+
+        preparePlayer(track?.previewUrl)
+        playButton.setOnClickListener{
+            playbackControl()
+        }
+        pauseButton.setOnClickListener{
+            playbackControl()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+    }
+
+    private fun preparePlayer(url : String?) {
+        mediaPlayer.setDataSource(url)
+        mediaPlayer.prepareAsync()
+        mediaPlayer.setOnPreparedListener {
+            playButton.isVisible = true
+            pauseButton.isVisible = false
+            playerState = STATE_PREPARED
+        }
+        mediaPlayer.setOnCompletionListener {
+            playButton.isVisible = true
+            pauseButton.isVisible = false
+            playerState = STATE_PREPARED
+            handler.removeCallbacks(trackProgressRunnable)
+            trackProgress.text = R.string.zero_track_progress.toString()
+        }
+    }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        playButton.isVisible = false
+        pauseButton.isVisible = true
+        playerState = STATE_PLAYING
+        handler.post(trackProgressRunnable)
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        playButton.isVisible = true
+        pauseButton.isVisible = false
+        playerState = STATE_PAUSED
+    }
+
+    private fun playbackControl() {
+        when(playerState) {
+            STATE_PLAYING -> {
+                pausePlayer()
+            }
+            STATE_PREPARED, STATE_PAUSED -> {
+                startPlayer()
+            }
+        }
+    }
+
+    private val trackProgressRunnable = object : Runnable {
+        override fun run() {
+            trackProgress.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(mediaPlayer.currentPosition)
+            if(playerState == STATE_PLAYING)
+                handler.postDelayed(this, 300L)
+            else if (playerState == STATE_PAUSED)
+                handler.removeCallbacks(this)
+        }
     }
 }
--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -3,6 +3,8 @@ package com.example.playlistmaker
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
@@ -12,6 +14,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.ProgressBar
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
@@ -23,6 +26,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 class SearchActivity : AppCompatActivity() {
     private var currentSearchText = SEARCH_TEXT_EMPTY
 
+    private val handler = Handler(Looper.getMainLooper())
     // для работы поиска
     private val iTunesBaseUrl = "https://itunes.apple.com"
     private val retrofit = Retrofit.Builder()
@@ -38,6 +42,7 @@ class SearchActivity : AppCompatActivity() {
     lateinit var recyclerSearch : RecyclerView
     lateinit var searchErrorView : LinearLayout
     lateinit var internetErrorView : LinearLayout
+    lateinit var searchProgressBar : ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,6 +61,7 @@ class SearchActivity : AppCompatActivity() {
         searchErrorView = findViewById(R.id.searchErrorView)
         internetErrorView = findViewById(R.id.internetErrorView)
         recyclerSearch = findViewById(R.id.recyclerSearch)
+        searchProgressBar = findViewById(R.id.searchProgressBar)
         myAdapter = TrackAdapter(tracks, searchHistory)
         recyclerSearch.adapter = myAdapter
 
@@ -71,6 +77,7 @@ class SearchActivity : AppCompatActivity() {
             searchErrorView.isVisible = false
             internetErrorView.isVisible = false
             recyclerSearch.isVisible = false
+            searchProgressBar.isVisible = false
             tracks.clear()
             myAdapter.notifyDataSetChanged()
             historyAdapter.notifyDataSetChanged()
@@ -84,6 +91,7 @@ class SearchActivity : AppCompatActivity() {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 clearButton.visibility = clearButtonVisibility(s)
                 searchHistoryView.isVisible = (inputEditText.hasFocus() && s?.isEmpty() == true && searchHistory.isNotEmpty())
+                searchDebounce()
             }
 
             override fun afterTextChanged(s: Editable?) {
@@ -100,7 +108,7 @@ class SearchActivity : AppCompatActivity() {
         inputEditText.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 if(currentSearchText.isNotEmpty())
-                    search(currentSearchText)
+                    search()
             }
             false
         }
@@ -113,7 +121,7 @@ class SearchActivity : AppCompatActivity() {
 
         val refreshButton = findViewById<Button>(R.id.refresh_button)
         refreshButton.setOnClickListener{
-            search(currentSearchText)
+            search()
         }
 
         val clearHistoryButton = findViewById<Button>(R.id.clear_history_button)
@@ -124,16 +132,28 @@ class SearchActivity : AppCompatActivity() {
         }
     }
 
-    private fun search(text: String){
-        iTunesService.search(text).enqueue(object : Callback<TracksResponse>{
+    private val searchRunnable = Runnable { search() }
+
+    private fun searchDebounce() {
+        handler.removeCallbacks(searchRunnable)
+        handler.postDelayed(searchRunnable, 2000L)
+    }
+
+    private fun search(){
+        if(currentSearchText.isEmpty())
+            return
+        // начинаем поиск - прогрессБар
+        searchErrorView.isVisible = false
+        internetErrorView.isVisible = false
+        recyclerSearch.isVisible = false
+        searchProgressBar.isVisible = true
+
+        iTunesService.search(currentSearchText).enqueue(object : Callback<TracksResponse>{
             override fun onResponse(
                 call: Call<TracksResponse>,
                 response: Response<TracksResponse>
             ) {
-                // пустой экран - идёт поиск
-                searchErrorView.isVisible = false
-                internetErrorView.isVisible = false
-                recyclerSearch.isVisible = false
+                searchProgressBar.isVisible = false
 
                 if (response.code() == 200){
                     // успешный запрос
@@ -163,6 +183,7 @@ class SearchActivity : AppCompatActivity() {
 
             override fun onFailure(call: Call<TracksResponse>, t: Throwable) {
                 // всё плохо
+                searchProgressBar.isVisible = false
                 searchErrorView.isVisible = false
                 internetErrorView.isVisible = true
                 recyclerSearch.isVisible = false
@@ -191,33 +212,5 @@ class SearchActivity : AppCompatActivity() {
     companion object{
         const val SEARCH_STRING = "SEARCH_STRING"
         const val SEARCH_TEXT_EMPTY = ""
-        /*
-        val mock_list : ArrayList<Track> = arrayListOf(
-            Track("Smells Like Teen Spirit",
-                "Nirvana",
-                "5:01",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-            Track("Billie Jean",
-                "Michael Jackson",
-                "4:35",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-            Track("Stayin' Alive",
-                "Bee Gees",
-                "4:10",
-                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-            Track("Whole Lotta Love",
-                "Led Zeppelin",
-                "5:33",
-                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-            Track("Sweet Child O'Mine",
-                "Guns N' Roses",
-                "5:03",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"),
-            Track("Test_Placeholder Long_Long_Long_Long_Long_Long_Long_Name",
-                "Long info long long long long long long long long long long long",
-                "69:420",
-                "https://https://www.google.com/")
-        )
-        */
     }
 }
